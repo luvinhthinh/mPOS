ba821fbde386d043675baabf39e498a0edad50ea
luvinhthinh <luvinhthinh.mono@gmail.com>
2015-09-17 09:16:42 +0800

    Modify: add message function when transaction completes

--- js/helpers/utility.js
+++ js/helpers/utility.js
@@ -7,4 +7,19 @@
         return date.getDate() + '_' + (date.getMonth()+1) + '_' + date.getFullYear();
     };
 
+    definition.sendMessage = function(number, message){
+        //CONFIGURATION
+        var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
+            }
+        };
+
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
+        sms.send(number, message, options, success, error);
+    }
+
 })(window.pos.helper.utility);
