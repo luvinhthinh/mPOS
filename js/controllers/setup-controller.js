cc246d4f03f023ce8d275fcb7b40ac05a710af3e
luvinhthinh <luvinhthinh.mono@gmail.com>
2015-09-22 22:20:24 +0800

    Modify: move Report to menu item and increase font size

--- js/controllers/setup-controller.js
+++ js/controllers/setup-controller.js
@@ -1,8 +1,8 @@
 /**
  * Created by User on 9/21/2015.
  */
-(function(mainApp){
-    mainApp.controller('dropdownCtrl', function($scope) {
+(function(mainApp, views){
+    mainApp.controller('dropdownCtrl', function($scope, $modal) {
         $scope.status = {
             isopen: false
         };
@@ -12,5 +12,23 @@
             $event.stopPropagation();
             $scope.status.isopen = !$scope.status.isopen;
         };
+
+        function report(){
+            var modalInstance = $modal.open({
+                animation: false,
+                templateUrl: views.getHtmls().report,
+                controller: 'reportCtrl',
+                resolve: {
+
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+
+            }, function () { // if error
+            });
+        }
+
+        $scope.report = report;
     });
-})(window.pos.app.mainApp);
+})(window.pos.app.mainApp, window.pos.view);
